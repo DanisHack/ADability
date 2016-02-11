8f638174ce3811f0648f4c735846be4ee6f42184
DanisHack <danishmohd3610@gmail.com>
2016-02-11 05:26:12 +0000

    added index.js

---
+++ webapp/src/server/utils/index.js
@@ -0,0 +1,8 @@
+'use strict';
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require('./executor/executor'));
+__export(require('./requester/requester'));
+__export(require('./data-source-manager/data-source-manager'));
+__export(require('./file/file'));
